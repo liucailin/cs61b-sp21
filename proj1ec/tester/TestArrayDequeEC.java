86ec0d9e22a44024a549d727df75f0c9c925765e
liu <liu@work.com>
2022-11-22 19:06:19 +0800

    proj1 ec

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -4,6 +4,9 @@ import edu.princeton.cs.algs4.StdRandom;
 import org.junit.Test;
 import student.StudentArrayDeque;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.Assert.*;
 
 public class TestArrayDequeEC {
@@ -29,24 +32,42 @@ public class TestArrayDequeEC {
                 msg.append(String.format("addLast(%s)\n", val));
             } else if (op == 2) {
                 if (!student.isEmpty() && !solution.isEmpty()) {
+//                    msg.append(String.format("curDeque -> %s", getDequeString(student)));
                     msg.append("removeFirst()\n");
                     assertEquals(msg.toString(), solution.removeFirst(), student.removeFirst());
+                    msg.append(String.format("curDeque -> %s", getDequeString(student)));
                 }
             } else if (op == 3) {
                 if (!student.isEmpty() && !solution.isEmpty()) {
+//                    msg.append(String.format("curDeque -> %s", getDequeString(student)));
                     msg.append("removeLast()\n");
                     assertEquals(msg.toString(), solution.removeLast(), student.removeLast());
+                    msg.append(String.format("curDeque -> %s", getDequeString(student)));
                 }
             } else if (op == 4) {
                 if (!student.isEmpty() && !solution.isEmpty()) {
                     int index = StdRandom.uniform(0, solution.size());
                     msg.append(String.format("get(%s)\n", index));
+                    msg.append(String.format("curDeque -> %s", getDequeString(student)));
                     assertEquals(msg.toString(), solution.get(index), student.get(index));
                 }
             }
 
         }
 
+    }
+
+    private String getDequeString(StudentArrayDeque deque) {
+        PrintStream ps = System.out;
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(baos));
+
+        deque.printDeque();
+        System.out.flush();
+        System.setOut(ps);
+
+        return baos.toString();
     }
 
 }
